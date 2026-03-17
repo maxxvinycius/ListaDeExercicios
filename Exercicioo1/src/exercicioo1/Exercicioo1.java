4d1bd763db7646b8f252d0d5d13b8d1c8e65f9f4
maxxvinycius <maxvinycius36@gmail.com>
2026-03-17 14:42:50 -0300

    Add files via upload

---
+++ Exercicioo1/src/exercicioo1/Exercicioo1.java
@@ -0,0 +1,17 @@
+
+package exercicioo1;
+
+
+public class Exercicioo1 {
+
+    
+    public static void main(String[] args) {
+        
+        ProdutoTecnologia p1 = new ProdutoTecnologia("Notebook", "Dell", 3500, 10);
+        ProdutoTecnologia p2 = new ProdutoTecnologia("Mouse", "Logitech", 120, 50);
+
+        p1.exibirProduto();
+        p2.exibirProduto();
+    }
+    
+}
---
+++ Exercicioo1/src/exercicioo1/ProdutoTecnologia.java
@@ -0,0 +1,57 @@
+
+package exercicioo1;
+
+
+public class ProdutoTecnologia {
+    private String nomeProduto;
+    private String marca;
+    private double preco;
+    private int quantidadeEstoque;
+
+    public ProdutoTecnologia(String nomeProduto, String marca, double preco, int quantidadeEstoque) {
+        this.nomeProduto = nomeProduto;
+        this.marca = marca;
+        this.preco = preco;
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public void exibirProduto() {
+        System.out.println("Produto: " + nomeProduto);
+        System.out.println("Marca: " + marca);
+        System.out.println("Preço: " + preco);
+        System.out.println("Estoque: " + quantidadeEstoque);
+        System.out.println();
+}
+}
